7392c7904ba1eba7bc4a04436b0ad3bb566a41f8
logonrm <logonrm@fiap.com.br>
2017-12-13 21:54:16 -0200

    falta mostrar o pedido na ultima tela

--- app/src/main/java/br/com/fiap45401/pizzaria/ConfirmarPedidoActivity.java
+++ app/src/main/java/br/com/fiap45401/pizzaria/ConfirmarPedidoActivity.java
@@ -3,11 +3,19 @@ package br.com.fiap45401.pizzaria;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import br.com.fiap45401.pizzaria.models.Pedido;
+
 public class ConfirmarPedidoActivity extends AppCompatActivity {
+
+    private Pedido pedido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmar_pedido);
+
+        if (getIntent() != null) {
+            pedido = getIntent().getParcelableExtra("PEDIDO");
+        }
     }
 }
